01598bf8588d885683065a1fa68c07747a4be135
Cameron Taggart <cameron.taggart@gmail.com>
2012-08-13 23:29:44 -0700

    How to Add WebSharper JavaScript to an ASP.NET MVC Web App

---
+++ HolaMundo/HolaMundo.fs
@@ -0,0 +1,15 @@
+﻿namespace Hola
+module Mundo =
+    open IntelliFactory.WebSharper
+    open IntelliFactory.WebSharper.JavaScript
+
+    [<JavaScript>]
+    let rec Factorial n =
+        match n with
+        | 0 -> 1
+        | n -> n * Factorial (n - 1)
+
+    [<JavaScript>]
+    let AlertFactorial n =
+        let fact = Factorial n
+        "Factorial(" + n.ToString() + ") = " + fact.ToString() |> Alert
